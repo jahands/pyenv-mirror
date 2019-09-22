6a11bd74f2cb01f5695bd061944e6cd9c1ff1976
S0urceC0der <S0urc3C0d3r@gmail.com>
2019-09-22 23:26:40 +0800

    first commit

---
+++ build.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import json
+from urllib.parse import urlparse
+
+DATABASE = "api/database.json"
+
+def build_database():
+    current_path = os.path.realpath(os.path.dirname(__file__))
+    pyenv_path = "plugins/python-build/share/python-build"
+    version_path = os.path.join(current_path, "pyenv", pyenv_path)
+    database = {}
+    host_set = set()
+    for name in os.listdir(version_path):
+        file_path = os.path.join(version_path, name)
+        if os.path.isdir(file_path):
+            continue
+        # print("Read file " + file_path)
+        with open(file_path, "r") as f:
+            for line in f:
+                line = line.strip()
+                if not line.startswith("install_package"):
+                    continue
+                for block in line.split():
+                    block = block.strip('"')
+                    if not block.startswith("https"):
+                        continue
+                    url = block
+                    host = urlparse(url).netloc
+                    host_set.add(host)
+                    if host == "ftpmirror.gnu.org":
+                        url = url.replace("https://ftpmirror.gnu.org/", "https://mirrors.ustc.edu.cn/gnu")
+                    elif host == "www.python.org":
+                        url = url.replace("https://www.python.org/ftp/python/", "https://npm.taobao.org/mirrors/python/")
+                    elif host == "pypi.python.org":
+                        # url = url.replace("https://pypi.python.org/packages/source/", "")
+                        # url = "https://pypi.tuna.tsinghua.edu.cn/simple/" + url[url.find("/")+1:]
+                        # todo
+                        continue
+                    elif host == "www.openssl.org":
+                        # print(url)
+                        continue
+                    elif host == "github.com":
+                        # print(url)
+                        continue
+                    elif host == "www.stackless.com":
+                        # print(url)
+                        continue
+                    elif host == "bitbucket.org":
+                        # print(url)
+                        continue
+                    if "#" not in url:
+                        continue
+                    sha256 = url.rsplit("#", 1)[1]
+                    database[sha256] = url
+    print(host_set)
+    with open(DATABASE, "w") as f:
+        f.write(json.dumps(database))
+
+
+
+def main():
+    repo = "https://github.com/pyenv/pyenv.git"
+    cmd = "git clone --depth=1 " + repo
+    os.system(cmd)
+    build_database()
+
+if __name__ == "__main__":
+    main()
