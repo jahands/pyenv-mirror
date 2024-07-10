11c8dbb25b3a50aa4b6104b9ade7fb0d7fad9265
Jacob Hands <jhands@cloudflare.com>
2024-07-10 10:46:57 -0500

    Remove styles link

--- apps/api/src/about.tsx
+++ apps/api/src/about.tsx
@@ -57,7 +57,6 @@ const Layout: FC = (props) => {
 				<title>PyMirror</title>
 				<meta charset="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<link rel="stylesheet" href="styles.css" />
 				{/* <!-- Open Graph Meta Tags --> */}
 				<meta property="og:title" content="PyMirror" />
 				<meta
