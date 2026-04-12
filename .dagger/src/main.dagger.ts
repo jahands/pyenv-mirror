b7211f0482147ad8071e1f428d51161e89504fb5
Jacob Hands <jacob@jacobhands.com>
2026-04-12 09:06:03 -0500

    chore: switch to blacksmith

--- .dagger/src/main.dagger.ts
+++ .dagger/src/main.dagger.ts
@@ -81,7 +81,8 @@ export class PyenvMirror {
 			})
 
 			// install pnpm deps
-			.withMountedCache('/pnpm-store', dag.cacheVolume('pnpm-store'))
+			// disabled cache for now because we're running on Blacksmith which doesn't persist anyway
+			// .withMountedCache('/pnpm-store', dag.cacheVolume('pnpm-store'))
 			.withExec(sh('pnpm config set store-dir /pnpm-store'))
 			.withExec(sh('FORCE_COLOR=1 pnpm install --frozen-lockfile --child-concurrency=10'))
 
